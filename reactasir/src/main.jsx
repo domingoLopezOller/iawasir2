81a76fe549469059126c6c954e199318d03dd6f8
QWsudo apt-get upgradeer34ty56 <domingolopez.instituto@gmail.com>
2024-10-14 11:01:43 +0200

    SCSS y componentes jsx

--- reactasir/src/main.jsx
+++ reactasir/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Web from './Web'
+import {Prueba,Domingo} from './Prueba'
+import { Cabecera } from './Cabecera'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Prueba />
+    <Web />
+    <Domingo />
+    <Cabecera/>
   </StrictMode>,
 )
