6c44e5897aef994dae105fddda28f3995c7ccaa5
jazmin0618 <jazmincarlossc@gmail.com>
2025-06-17 09:06:10 +0200

    Q

--- reactasir/src/paginaWeb/Aside.jsx
+++ reactasir/src/paginaWeb/Aside.jsx
@@ -6,8 +6,8 @@ function Aside() {
   return (
     <aside>
       <BasicSelect/>
+      <h1>HOLA MUNDO</h1>
       <div>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/wGxDfSWC4Ww?si=M8v6MB5VOe5Z156H" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
     </aside>
   )
